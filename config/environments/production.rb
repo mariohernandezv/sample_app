5ce97b47fee032c914faea985d0e269432314e52
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-01-05 18:49:11 -0500

    Arreglando CSS en Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ SampleApp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( custom.css application.css static_pages.css users.css )
+  config.assets.precompile += %w( custom.css.scss application.css static_pages.css.scss users.css.scss )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
